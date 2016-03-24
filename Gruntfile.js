e0b4dc1e9cd3144b7d405ae248a0e17f056c18a1
uideas <uideas@163.com>
2016-03-24 23:53:26 +0800

    update

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          hostname: '127.0.0.1',
+          port: 8080,
+          base: '.'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect']);
+
+};
